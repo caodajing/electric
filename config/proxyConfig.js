c2d20a4c345afdc6002aa8314c7cb679ca36ccd4
caojing5 <caojing5@ucfgroup.com>
2020-04-16 11:26:17 +0800

    提交config、postcss文件
    提交config、postcss文件

---
+++ config/proxyConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  proxy: {
+    '/zigbee': {    //将www.exaple.com印射为/apis
+      target: 'http://shcloud2.wmtechzone.club:12005',  // 接口域名
+      secure: false,  // 如果是https接口，需要配置这个参数
+      changeOrigin: true,  //是否跨域
+      pathRewrite: {
+        '^/apis': '' //需要rewrite的,
+      }
+    }
+  }
+}
